8f7f0fb46d5949ed5f10c5c837201fcb47a0bcd4
Sheri Brown <sherib70@gmail.com>
2022-06-20 19:50:39 -0400

    CREATED NEW PRODUCT (router.post =try, await/creat, if, map, else, catch

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -44,6 +44,25 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
+    try{
+      const newProduct = await Product.create(req.body);
+        if(req.body.tagIds.length){
+          const productTagIdArr = req.body.tagIds.map((tag_id) => {
+                return {
+                  product_id: newProduct.id,
+                  tag_id,
+                };
+            });
+            const productTagIds =  await ProductTag.bulkCreate(productTagIdArr);
+            res.status(200).json(productTagIds);
+          }else{
+            res.status(200).json(newProduct);
+          }
+        }catch(err){
+          res.status(400).json(err);
+        }
+
+
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
