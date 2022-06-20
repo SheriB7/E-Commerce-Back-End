758849e3cf25ecdef705433da822e260db2b02fd
Sheri Brown <sherib70@gmail.com>
2022-06-20 17:24:18 -0400

    find one categor by its id value and included its associated Products. (get=try, if, catch)

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -18,6 +18,20 @@ router.get('/', async (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+try {
+    const categorieData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!categorieData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categorieData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
