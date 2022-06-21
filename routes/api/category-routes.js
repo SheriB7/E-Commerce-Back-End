7807c4f25563fc1cfbc37a106df1cf81770640e7
Sheri Brown <sherib70@gmail.com>
2022-06-21 16:42:52 -0400

    updated routes catagory

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,12 +6,12 @@ const { Category, Product } = require('../../models');
 router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  try{
-    const categoriesData = await Category.findAll({ include: [{model:Product}]
+  try {
+    const categoriesData = await Category.findAll({
     })
     res.status(200).json(categoriesData);
   }
-  catch(err){
+  catch (err) {
     res.status(400).json(err)
   }
 });
@@ -19,7 +19,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-try {
+  try {
     const categorieData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }],
     });
@@ -37,47 +37,49 @@ try {
 
 router.post('/', (req, res) => {
   // create a new category
-  try{
+  try {
     const newCategoryData = await Category.create(req.body)
     res.status(200).json(newCategoryData);
   }
-  catch(err){
+  catch (err) {
     res.status(400).json(err)
   }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-  try{
-    const updateCategoryData = await Category.update(req.body, 
-      {where: {id: req.params.id,
-      },
-    });
+  try {
+    const updateCategoryData = await Category.update(req.body,
+      {
+        where: {
+          id: req.params.id,
+        },
+      });
     if (!updateCategoryData[0]) {
       res.status(404).json({ message: 'Category does not exist with this id!' });
       return;
     }
     res.status(200).json(updateCategoryData);
   }
-  catch(err){
+  catch (err) {
     res.status(400).json(err)
   }
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
-  try{
+  try {
     const deteCategory = await Category.destroy({
-    where: {
-    id: req.params.id,
-    },
+      where: {
+        id: req.params.id,
+      },
     });
     if (!deteCategory) {
       res.status(404).json({ message: 'Category does not exist with this id!' });
       return;
     }
     res.status(200).json(deteCategory);
-  }catch(err){
+  } catch (err) {
     res.status(400).json(err)
   }
 });
